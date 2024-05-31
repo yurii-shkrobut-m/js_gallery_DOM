08cad129129a6d9ba0006e7be4d6259548550616
Yurii Shkrobut <georgebest2409@gmail.com>
2024-05-31 11:56:57 +0300

    make build

---
+++ index.8080d7bf.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.querySelector(".gallery__list").addEventListener("click",function(t){t.preventDefault();var l=t.target.closest(".list-item__link");e.src=l.href});
+//# sourceMappingURL=index.8080d7bf.js.map
